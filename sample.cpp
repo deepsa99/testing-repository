049cff0800b465f9616626e8d6c11d76afdd959b
deepsa99 <learningswithds@gmail.com>
2024-01-15 23:12:03 +0530

    my first git commit in test branch

---
+++ sample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You entered hey boobie uiufdfhduwkhfuiqwhirfhu2i2" << number;    
+    return 0;
+}
+
